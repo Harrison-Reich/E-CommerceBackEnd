d077b51a22ff0f0685fce2dac77438b24bc6df14
Harrison Reich <hreich1998@gmail.com>
2021-12-21 23:07:35 -0800

    made async functions

--- routes/category-routes.js
+++ routes/category-routes.js
@@ -3,25 +3,27 @@ const { Category, Product } = require('../models')
 
 // The `/api/categories` endpoint
 
-router.get('/categories', (req, res) => {
+router.get('/categories', async function (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const categories = await Category.findAll({include: [Product]})
+  res.json(categories)
 })
 
-router.get('/categories/:id', (req, res) => {
+router.get('/categories/:id', async function (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 })
 
-router.post('/categories', (req, res) => {
+router.post('/categories', async function (req, res) => {
   // create a new category
 })
 
-router.put('/categories/:id', (req, res) => {
+router.put('/categories/:id', async function (req, res) => {
   // update a category by its `id` value
 })
 
-router.delete('/categories/:id', (req, res) => {
+router.delete('/categories/:id', async function (req, res) => {
   // delete a category by its `id` value
 })
 
